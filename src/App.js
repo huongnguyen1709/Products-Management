7a2c0d97eba991f91ed7ac276d1ba1d779d1a586
Huong Nguyen <huong.nguyen@edu.turkuamk.fi>
2020-03-11 16:41:41 +0200

    divide into components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/Menu/Menu.js';
+import ProductList from './components/ProductList/ProductList';
 
 class App extends Component {
     render() {
         return (
 
             <div>
-                <div className="navbar navbar-default">
-                    <a className="navbar-brand">Call API</a>
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <a>Trang Chủ</a>
-                        </li>
-                        <li>
-                            <a>Quản Lý Sản Phẩm</a>
-                        </li>
-                    </ul>
-                </div>
+                <Menu />
                 
                 <div className="container">
                     
                     <div className="row">
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             
-                            <button type="button" className="btn btn-info mb-10">Thêm Sản Phẩm</button>                           
-                            
-                            <div className="panel panel-primary">
-                                  <div className="panel-heading">
-                                        <h3 className="panel-title">Danh Sách Sản Phẩm</h3>
-                                  </div>
-                                  <div className="panel-body">
-                                        
-                                        <table className="table table-bordered table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>STT</th>
-                                                    <th>Mã</th>
-                                                    <th>Tên</th>
-                                                    <th>Giá</th>
-                                                    <th>Trạng Thái</th>
-                                                    <th>Hành Động</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>1</td>
-                                                    <td>Iphone 6 Plus</td>
-                                                    <td>500</td>
-                                                    <td> 
-                                                        <span className="label label-warning">Còn Hàng</span>
-                                                    </td>
-                                                    <td>
-                                                        <button type="button" className="btn btn-success mr-10">Sửa</button>
-                                                        <button type="button" className="btn btn-danger">Xóa</button>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                        
-                                  </div>
-                            </div>
+                            <button type="button" className="btn btn-info mb-10">Thêm Sản Phẩm</button>             
+                            <ProductList />
                             
                         </div>
                     </div>
